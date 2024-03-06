f6e65e55facbd479746a810f9c7bb2f648220400
Woo Kai Kein <kaikein.woo@gmail.com>
2024-03-06 15:58:35 +0800

    Initialized server and client

---
+++ servergainz/src/main/java/backend/servergainz/ServergainzApplication.java
@@ -0,0 +1,13 @@
+package backend.servergainz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServergainzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServergainzApplication.class, args);
+	}
+
+}
---
+++ servergainz/src/test/java/backend/servergainz/ServergainzApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.servergainz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServergainzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
